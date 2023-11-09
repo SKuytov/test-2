7d5d89af9815c03ef12c5cd364fa2123bf4312e2
SKuytov <138648456+SKuytov@users.noreply.github.com>
2023-11-09 05:08:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 // Here is a mockup example for the first few IDs
 const doorCoordinates = {
     1: { building: 'building1', x: 42.4, y: 89 },
-    2: { building: 'building1', x: 45.3, y: 89.5 }
-    // ... Add all your coordinates here
+    2: { building: 'building1', x: 45.3, y: 89.5 },
+    3: { building: 'building1', x: 44.75, y: 88.46 },
+    4: { building: 'building1', x: 44.6 ,y: 87.0},
+    5: { building: 'building1', x: 46.91, y: 88.31  },
+    6: { building: 'building1', x: 52.94, y: 88.56 },
+    7: { building: 'building1', x: 47.92, y: 85.95 },
+    8: { building: 'building1', x: 48.65, y: 85.49 },
+    9: { building: 'building1', x: 50.08, y: 86.18 },
+    10: { building: 'building1', x: 51.42 ,y: 85.41 },
+    // rest coordinates here
   };
   
   document.getElementById('locateBtn').addEventListener('click', function() {
